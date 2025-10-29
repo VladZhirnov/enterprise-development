19b7cded7473d62475a5ef613a8f7d4ebd0480f0
Влад Жирнов <vlad-zhirnov-1995@mail.ru>
2025-10-29 16:18:27 +0400

    first commit

---
+++ Bikes/Bikes.Domain/Enum/BikeType.cs
@@ -0,0 +1,37 @@
+﻿namespace Bikes.Domain.Enum;
+
+/// <summary>
+/// Type of bike
+/// </summary>
+public enum BikeType
+{
+    /// <summary>
+    /// Road bike
+    /// </summary>
+    Road,
+
+    /// <summary>
+    /// Mountain bike
+    /// </summary>
+    Mountain,
+
+    /// <summary>
+    /// Hybrid bike
+    /// </summary>
+    Hybrid,
+
+    /// <summary>
+    /// Sport bike
+    /// </summary>
+    Sport,
+
+    /// <summary>
+    /// Electric bike
+    /// </summary>
+    Electric,
+
+    /// <summary>
+    /// BMX bike
+    /// </summary>
+    BMX
+}
